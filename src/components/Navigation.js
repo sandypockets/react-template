9ea514496f4e26fc8ae32d45129a64a84047512c
sandypockets <zac@znacreative.com>
2021-04-11 22:38:27 -0400

    Add placeholder text to nav

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navigation.css';
+
+export const Navigation = () => {
+  return(
+    <nav className="nav">
+      <span id="logo-type">App Name</span>
+      <span className="nav-span-text">Some Text</span>
+    </nav>
+  )
+}
